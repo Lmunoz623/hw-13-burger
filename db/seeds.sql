e9c689b0cdd155c7a88bcd4cb233bf3d1e403464
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-10-18 21:23:22 -0500

    created burgers sql database and seeds folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Nashville Hot N Spicy Crispy Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Crabby Patty');
