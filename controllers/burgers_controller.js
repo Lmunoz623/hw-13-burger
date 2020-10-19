6cbc6d602d1184eaec89c785ddb3389b56309a13
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-10-18 22:11:26 -0500

    added controllers folder and boiler plate code in js file in controllers folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+
+
+
+router.get("", function(req, res) {
+
+});
+  
+router.post("", function(req, res) {
+
+});
+  
+router.put("", function(req, res) {
+
+});
+
+
+
+
+
+module.exports = router;
