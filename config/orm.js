0a6cdfe3620fd99fae0116ae4cde4e3f6597e7d1
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-10-18 21:54:58 -0500

    Added boiler plate for orm.js and built file for config folder

---
+++ config/orm.js
@@ -0,0 +1,23 @@
+var connection = require("../config/connection.js");
+
+// CODE BELOW //
+
+var orm = {
+    all: function() {
+   
+
+    },
+    create: function() {
+      
+
+    },
+    update: function() {
+
+
+    }
+  };
+
+
+// CODE ABOVE //
+
+module.exports = orm;
