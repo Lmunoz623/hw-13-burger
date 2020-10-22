d51d72ce4d06ddc62401c095717198a6ba0c34ce
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-10-21 23:48:44 -0500

    completed code, app not running yet

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newDevoured");
+  
+      var eaten = {
+        devoured: newDevoured
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: eaten
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burg").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+        }
+      );
+    });
+  });
