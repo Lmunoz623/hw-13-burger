01f07b572c31369acd901c19a7d812d583b4de03
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-10-22 00:05:03 -0500

    changed connection.js and added Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQLPW,
+        database: "burgers_db"
+      });
+}
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,12 +20,7 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+
 
 module.exports = connection;
